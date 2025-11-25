05b9a086e44802bfc1c8bd0a523c2108b778f26d
madhavikora469 <madhavikora6@gmail.com>
2025-11-25 10:54:47 +0530

    Add files via upload

---
+++ sentence into words.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	char a[30];
+	int i,v=0,c=0,d=0,s=0;
+	scanf("%[^\n]s" , a);
+	for (i=0;a[i]!='\0';i++)
+	{
+		if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u')
+	    v++;
+		else if (a[i]==' ')
+		s++;
+		
+	}
+	return 0;
+	
+}
